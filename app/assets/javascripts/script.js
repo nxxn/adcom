d973566df0f84dcc19b527aa047ec58270f2daf6
Nikita Gnotov <nikita@gnotov.com>
2017-05-03 05:54:15 +0300

    add new items to homepage

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -15,5 +15,58 @@ $(document).ready(function() {
       }
   });
 
+  $('.portfolio_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    prevArrow: '<div class="slick-prev slider_arrow slider_left_arrow"><i class="fa fa-angle-left"></i></div>',
+    nextArrow: '<div class="slick-next slider_arrow slider_right_arrow"><i class="fa fa-angle-right"></i></div>'
+    // responsive: [
+    //   {
+    //     breakpoint: 1200,
+    //     settings: {
+    //       prevArrow: '<div class="slick-prev arrow left_arrow"></div>',
+    //       nextArrow: '<div class="slick-next arrow right_arrow"></div>',
+    //       centerMode: true,
+    //       slidesToShow: 1
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 550,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+
+  });
+
+  $('.testimonial_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    prevArrow: '<div class="slick-prev slider_arrow testimonial_left_arrow"><i class="fa fa-angle-left"></i></div>',
+    nextArrow: '<div class="slick-next slider_arrow testimonial_right_arrow"><i class="fa fa-angle-right"></i></div>'
+    // responsive: [
+    //   {
+    //     breakpoint: 1200,
+    //     settings: {
+    //       prevArrow: '<div class="slick-prev arrow left_arrow"></div>',
+    //       nextArrow: '<div class="slick-next arrow right_arrow"></div>',
+    //       centerMode: true,
+    //       slidesToShow: 1
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 550,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+
+  });
 
 });
