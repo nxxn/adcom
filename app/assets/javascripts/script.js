9788745ad87c805c3471ede7260c5f13ba355320
Nikita Gnotov <nikita@gnotov.com>
2017-05-07 06:31:36 +0300

    add video modal

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
 
-  $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-          || location.hostname == this.hostname) {
+  // Center modal
+  function centerModals(){
+    $('.modal').each(function(i){
+      var $clone = $(this).clone().css('display', 'block').appendTo('body');
+      var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+      top = top > 0 ? top : 0;
+      $clone.remove();
+      $(this).find('.modal-content').css("margin-top", top);
+    });
+  }
+  $('.modal').on('show.bs.modal', centerModals);
+  $(window).on('resize', centerModals);
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-             if (target.length) {
-               $('html,body').animate({
-                   scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
+  var video_url = $("#video_modal").attr('src');
+
+  $("#videoModal").on('hide.bs.modal', function(){
+    $("#video_modal").attr('src', '');
+  });
+
+  $("#videoModal").on('show.bs.modal', function(){
+    $("#video_modal").attr('src', video_url);
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+       if (target.length) {
+         $('html,body').animate({
+           scrollTop: target.offset().top
+        }, 1000);
+        return false;
       }
+    }
   });
 
   $('.portfolio_slider').slick({
