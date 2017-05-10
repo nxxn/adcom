eb00e12f6c6d73a09be1921683ce7ef26c8bfed5
Nikita Gnotov <nikita@gnotov.com>
2017-05-10 06:38:24 +0300

    add responsiveness

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -42,26 +42,23 @@ $(document).ready(function() {
     infinite: true,
     slidesToShow: 1,
     prevArrow: '<div class="slick-prev slider_arrow slider_left_arrow"><i class="fa fa-angle-left"></i></div>',
-    nextArrow: '<div class="slick-next slider_arrow slider_right_arrow"><i class="fa fa-angle-right"></i></div>'
-    // responsive: [
-    //   {
-    //     breakpoint: 1200,
-    //     settings: {
-    //       prevArrow: '<div class="slick-prev arrow left_arrow"></div>',
-    //       nextArrow: '<div class="slick-next arrow right_arrow"></div>',
-    //       centerMode: true,
-    //       slidesToShow: 1
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 550,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       slidesToShow: 1
-    //     }
-    //   }
-    // ]
+    nextArrow: '<div class="slick-next slider_arrow slider_right_arrow"><i class="fa fa-angle-right"></i></div>',
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
 
   });
 
@@ -69,26 +66,23 @@ $(document).ready(function() {
     infinite: true,
     slidesToShow: 1,
     prevArrow: '<div class="slick-prev slider_arrow testimonial_left_arrow"><i class="fa fa-angle-left"></i></div>',
-    nextArrow: '<div class="slick-next slider_arrow testimonial_right_arrow"><i class="fa fa-angle-right"></i></div>'
-    // responsive: [
-    //   {
-    //     breakpoint: 1200,
-    //     settings: {
-    //       prevArrow: '<div class="slick-prev arrow left_arrow"></div>',
-    //       nextArrow: '<div class="slick-next arrow right_arrow"></div>',
-    //       centerMode: true,
-    //       slidesToShow: 1
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 550,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       slidesToShow: 1
-    //     }
-    //   }
-    // ]
+    nextArrow: '<div class="slick-next slider_arrow testimonial_right_arrow"><i class="fa fa-angle-right"></i></div>',
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
 
   });
 
